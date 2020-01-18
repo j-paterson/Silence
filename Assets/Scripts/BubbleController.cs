1f0d9b51ee2249ae0d423ba0521a8d705f83ee74
Jesse Paterson <jesse.paterson@strivr.com>
2020-01-17 22:57:11 -0500

    Add bubble

--- Assets/Scripts/BubbleController.cs
+++ Assets/Scripts/BubbleController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[ExecuteInEditMode]
+
 public class BubbleController : MonoBehaviour
 {
     public Material quillPaintMat;
@@ -15,6 +15,17 @@ public class BubbleController : MonoBehaviour
         bubbles.Add(new Vector4(0,0,0,0.1f));
         bubbles.Add(new Vector4(0, 1, 0, 0.1f));
         bubbles.Add(new Vector4(0, 0, 1, 0.1f));
+
+        cBuffer = new ComputeBuffer(bubbles.Count, sizeof(float) * 4);
+        cBuffer.SetData(bubbles);
+
+        quillPaintMat.SetBuffer("sBuffer", cBuffer);
+        quillPaintMat.SetFloat("numBubbles", bubbles.Count);
+    }
+
+    void addBubble(Vector4 bubble)
+    {
+        bubbles.Add(bubble);
 
         cBuffer = new ComputeBuffer(bubbles.Count, sizeof(float) * 4);
         cBuffer.SetData(bubbles);
