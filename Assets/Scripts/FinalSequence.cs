f2b22afd866382f48429f88b0ba710bfd240b0e5
Crobertson9360 <45612458+Crobertson9360@users.noreply.github.com>
2020-01-19 11:52:30 -0500

    Env updates newstuff3

--- Assets/Scripts/FinalSequence.cs
+++ Assets/Scripts/FinalSequence.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class FinalSequence : MonoBehaviour
 {
-    public Animator shadowAnim; 
+    public GameObject shadow;
+
+    Animator anim;
+
     public GameObject ImpactPoint;
 
     public float transitionTime = 0.1f;
 
     public bool readyForNextPhase = false;
+
+    public int timesNeeded = 0;
+
+    public AudioSource ending;
+
+    public AudioSource noiseLight;
+
+    bool canHit = false;
+
+    bool hasStartedAnim = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,25 +36,54 @@ public class FinalSequence : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if( timesNeeded >= 10 && hasStartedAnim == false)
+        {
 
+            hasStartedAnim = true;
+            // break glass, fall down.
+
+            anim.CrossFade("Fallen", 0.1f);
+
+            //anim.SetBool("mirrorBreak", true);
+
+            print("Animating!");
+
+
+            ending.PlayDelayed(2.0f);
+
+        }
     }
 
     void StartBattle()
     {
-        print("Restarting Battle");
-        //slimeAnim.CrossFade("IdleBattle", transitionTime);
-        //spikeAnim.CrossFade("IdleBattle", transitionTime);
+        anim = shadow.GetComponent<Animator>();
 
-        StartCoroutine(WaitForTransition(3.0f));
+        StartCoroutine(WaitForTransition(5.0f));
 
     }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject voiceMagic = other.gameObject;
+
+        if (voiceMagic.CompareTag("Voice Magic") && canHit == true)
+        {
+            timesNeeded++;
+            print(timesNeeded);
+        }
+    }
+
+
 
     IEnumerator WaitForTransition(float secondsNeeded)
     {
         yield return new WaitForSeconds(secondsNeeded);
         //slimeAnim.CrossFade("Attack01", transitionTime);
         //spikeAnim.CrossFade("GetHit", transitionTime);
-        StartBattle();
+        ImpactPoint.SetActive(true);
+
+        canHit = true;
     }
 
 }
