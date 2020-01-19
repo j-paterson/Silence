6670ee1e16af917c70187ef6d5961664acf72d0c
Crobertson9360 <45612458+Crobertson9360@users.noreply.github.com>
2020-01-18 22:48:45 -0500

    Env updates 3

---
+++ Assets/Sounds/AreaTriggerForSounds.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTriggerForSounds : MonoBehaviour
+{
+
+    public GameObject shadowMonster;
+
+    public AudioSource source;
+
+    bool hasPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (shadowMonster.activeSelf == true)
+        {
+            shadowMonster.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject player = other.gameObject;
+
+        if(player.name.Contains("Player") && hasPlayed == false)
+        {
+            source.Play();
+            hasPlayed = true;
+            shadowMonster.SetActive(true);
+        }
+    }
+
+}
