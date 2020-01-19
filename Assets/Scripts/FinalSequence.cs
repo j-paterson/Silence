b05394f964f608636032545f176e7b77e2d148a7
Jesse Paterson <jesse.paterson@strivr.com>
2020-01-19 10:15:54 -0500

    Audio added, working on post processing bloom

---
+++ Assets/Scripts/FinalSequence.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalSequence : MonoBehaviour
+{
+    public Animator shadowAnim;
+
+    public float transitionTime = 0.1f;
+
+    public bool readyForNextPhase = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //slimeAnim = GetComponent<Animator>();
+        //spikeAnim = GetComponent<Animator>();
+
+        StartBattle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void StartBattle()
+    {
+        print("Restarting Battle");
+        //slimeAnim.CrossFade("IdleBattle", transitionTime);
+        //spikeAnim.CrossFade("IdleBattle", transitionTime);
+
+        StartCoroutine(WaitForTransition(3.0f));
+
+    }
+
+    IEnumerator WaitForTransition(float secondsNeeded)
+    {
+        yield return new WaitForSeconds(secondsNeeded);
+        //slimeAnim.CrossFade("Attack01", transitionTime);
+        //spikeAnim.CrossFade("GetHit", transitionTime);
+        StartBattle();
+    }
+
+}
+
