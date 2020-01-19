75934f834ff2c42041baec86148e8eb72f78311b
Crobertson9360 <45612458+Crobertson9360@users.noreply.github.com>
2020-01-19 10:53:14 -0500

    Env updates newstuff

---
+++ Assets/Scripts/SecondAreaScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondAreaScript : MonoBehaviour
+{
+    public GameObject squiggly;
+    public GameObject squiggly2;
+
+    public GameObject squigglyRunning;
+    public GameObject squigglyRunning2;
+
+    public bool changedToPositive = false;
+
+    public GameObject shadowMonster;
+
+    public int timesNeeded = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (squigglyRunning.activeSelf || squigglyRunning2.activeSelf)
+        {
+            squigglyRunning.SetActive(false);
+            squigglyRunning2.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timesNeeded >= 10)
+        {
+            changedToPositive = true;
+
+            squiggly.SetActive(false);
+            squiggly2.SetActive(false);
+
+            squigglyRunning.SetActive(true);
+            squigglyRunning2.SetActive(true);
+
+            shadowMonster.SetActive(false);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject voiceMagic = other.gameObject;
+
+        if (voiceMagic.CompareTag("Voice Magic") && changedToPositive == false)
+        {
+            timesNeeded++;
+        }
+    }
+}
---
+++ Assets/Scripts/VillagersAndShadow.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagersAndShadow : MonoBehaviour
+{
+    public GameObject ManFighting;
+    public GameObject WomanFighting;
+
+    public GameObject crowdMember1;
+    public GameObject crowdMember2;
+    public GameObject crowdMember3;
+
+    public bool changedToPositive = false;
+
+    public GameObject shadowMonster;
+
+    public int timesNeeded = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timesNeeded >= 10 && changedToPositive == false)
+        {
+            changedToPositive = true;
+
+            ManFighting.SetActive(false);
+            WomanFighting.SetActive(false);
+
+            shadowMonster.SetActive(false);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject voiceMagic = other.gameObject;
+
+        if (voiceMagic.CompareTag("Voice Magic") && changedToPositive == false)
+        {
+            timesNeeded++;
+        }
+    }
+}
